7ddd7604a8c57934870f8c4f288538088534caf4
Bifrost <nkeyani-@student.42barcelona.com>
2023-10-26 16:59:19 +0200

    fix: refactor

--- CPP0/ex00/megaphone.cpp
+++ CPP0/ex00/megaphone.cpp
@@ -13,12 +13,12 @@
 
 int main(int argc, char **argv)
 {
-    if (argc < 2){
+    if (argc < 2) {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    } else{
+    } else {       
         for (size_t i = 1; argv[i]; i++){
             std::string megaphone = argv[i];
-            for (size_t j = 0; j < megaphone.length(); j++){
+            for (size_t j = 0; j < megaphone.length(); j++) {
                 megaphone[j] = toupper(megaphone[j]);
             }
             std::cout << megaphone;
