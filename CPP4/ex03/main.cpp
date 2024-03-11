2dafd386ae1e8d145c71eb55c4909e113882dbf0
Bifrost <nkeyani-@student.42barcelona.com>
2024-03-11 18:46:36 +0100

    refactor: main

--- CPP4/ex03/main.cpp
+++ CPP4/ex03/main.cpp
@@ -18,68 +18,6 @@
 
 int main()
 {	
-
-	{
-	AMateria *a = new Ice();
-	std::cout << a->getType() << std::endl;
-	delete a;
-	a = new Cure();
-	std::cout << a->getType() << std::endl;
-	delete a;
-	std::cout << "--------------------------------" << std::endl;
-	}
-
-	{
-		IMateriaSource* src = new MateriaSource();
-		src->learnMateria(new Ice());
-		src->learnMateria(new Ice());
-		std::cout << "------print materias src----- " << std::endl;
-		src->printMaterias();
-
-		IMateriaSource *src_copy = new MateriaSource(*dynamic_cast<MateriaSource*>(src));;
-		std::cout << "------print materias src copy----- " << std::endl;
-		src_copy->printMaterias();
-
-		src->learnMateria(new Ice());
-		src_copy->learnMateria(new Cure());
-
-		std::cout << "------print materias src - deep copy check----- " << std::endl;
-		src->printMaterias();
-
-		std::cout << "------print materias srccopy - deep copy check----- " << std::endl;
-		src_copy->printMaterias();
-
-		delete src;
-		delete src_copy;
-	}
-
-	{
-		std::cout << "--------------------------------" << std::endl;
-		ICharacter *me = new Character("me");
-		AMateria *ice = new Ice();
-		AMateria *cure = new Cure();
-		me->equip(ice);
-		me->equip(cure);
-		std::cout << "------------original inventory--------------------" << std::endl;
-		me->printInventory();
-
-		ICharacter *me_copy = new Character(*dynamic_cast<Character*>(me));
-		me->unequip(0);
-
-		std::cout << "------------original inventory--------------------" << std::endl;
-		me->printInventory();
-
-		std::cout << "------------copy inventory--------------------" << std::endl;
-		me_copy->printInventory();
-
-		delete me;
-		delete me_copy;
-		delete ice;
-		delete cure;
-		
-	}
-	
-	{
 	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
@@ -110,6 +48,6 @@ int main()
 	delete bob;
 	delete me; 
 	delete src;
-	}
+
 	return 0;
 }
